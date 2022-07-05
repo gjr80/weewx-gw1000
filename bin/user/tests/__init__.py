ad430d1ea08ea961d76d0876e331d1c5c9b3a8c1
gjr80 <gjroderick@gmail.com>
2022-07-05 14:48:07 +1000

    - updated for current release

--- bin/user/tests/__init__.py
+++ bin/user/tests/__init__.py
@@ -1,10 +1,10 @@
 """
-Test suite package for the WeeWX GW1000 driver.
+Test suite package for the WeeWX Ecowitt gateway device driver.
 
 This file is included to allow the test suite to operate under Python 3.2 and
 earlier.
 
-Copyright (C) 2021 Gary Roderick                        gjroderick<at>gmail.com
+Copyright (C) 2021-22 Gary Roderick                     gjroderick<at>gmail.com
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -18,9 +18,11 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with
 this program.  If not, see https://www.gnu.org/licenses/.
 
-Version: 0.4.0                                      Date: 27 September 2021
+Version: 0.5.0                                      Date: ?? July 2022
 
 Revision History
+    ?? July 2022            v0.5.0
+        -   updated for Ecowitt gateway driver v0.5.0 release
     27 September 2021       v0.4.0
         -   initial release.
 """
